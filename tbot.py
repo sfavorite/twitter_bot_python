67e439dc47262e163ecbe7f3168a8052b72e969e
Scot Favorite <scot.favorite@gmail.com>
2017-08-15 10:40:53 -0400

    Access variables from .env

--- tbot.py
+++ tbot.py
@@ -1,1 +1,12 @@
 from twython import Twython
+from dotenv import load_dotenv, find_dotenv
+import os
+
+# Load key, token, secrets from .env
+load_dotenv(find_dotenv())
+
+# Twitter keys, etc we need 
+consumer_key        = os.environ.get("consumer_key")
+consumer_secret     = os.environ.get("consumer_secret")
+access_token        = os.environ.get("access_token")
+access_token_secret = os.environ.get("access_token_secret")
