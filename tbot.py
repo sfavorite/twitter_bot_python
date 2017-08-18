481b7c4824736ab53103834bc1deefea625d63ef
Scot Favorite <scot.favorite@gmail.com>
2017-08-18 07:55:36 -0400

    Message with media

--- tbot.py
+++ tbot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from twython import Twython
 from dotenv import load_dotenv, find_dotenv
 import os
@@ -5,8 +7,25 @@ import os
 # Load key, token, secrets from .env
 load_dotenv(find_dotenv())
 
-# Twitter keys, etc we need 
+# Twitter keys, etc we need
 consumer_key        = os.environ.get("consumer_key")
 consumer_secret     = os.environ.get("consumer_secret")
 access_token        = os.environ.get("access_token")
 access_token_secret = os.environ.get("access_token_secret")
+
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+message = 'Hi'
+
+twitter.update_status(status=message)
+
+message = "New picture"
+
+photo = open("random.jpg", 'rb')
+
+twitter.update_status_with_media(status=message, media=photo)
